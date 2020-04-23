7a85218bb3c8459c1d554c8e57329584b723ff20
LQuangTien <tienkg5554@gmail.com>
2020-04-23 14:29:50 +0700

    Env variable

--- 1-helloExpress/index.js
+++ 1-helloExpress/index.js
@@ -1,4 +1,7 @@
 //package
+require('dotenv').config()
+console.log(process.env.SESSION_SECRET)
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -13,7 +16,7 @@ const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser('asdasdasd'))
+app.use(cookieParser(process.env.SESSION_SECRET))
 
 app.set('view engine', 'pug')
 app.set('views','./views')

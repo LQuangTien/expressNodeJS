00ca394ff3390d539050dcd371c79258e1da9ca2
LQuangTien <tienkg5554@gmail.com>
2020-04-27 10:30:49 +0700

    install mongodb and mongoose package

--- 1-helloExpress/controllers/product.controller.js
+++ 1-helloExpress/controllers/product.controller.js
@@ -1,13 +1,17 @@
-var db = require('../db')
+var Product = require("../models/product.model")
 
-module.exports.index = function(req, res){
-    var page = parseInt(req.query.page) || 1
-    var perPage = 8;
-    
-    var start = (page - 1) * perPage
-    var end = page * perPage
-
-    res.render('product/index',{
-        products: db.get('products').value().slice(start, end)
+module.exports.index = async function(req, res){
+//    var page = parseInt(req.query.page) || 1
+//    var perPage = 8;
+//    
+//    var start = (page - 1) * perPage
+//    var end = page * perPage
+//
+//    res.render('product/index',{
+//        products: db.get('products').value().slice(start, end)
+//    })
+    var products = await Product.find()
+    res.render('product/index', {
+        products: products
     })
 }
